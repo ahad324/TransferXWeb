0fc069ab926801a38f227ede31e665b39f1f28e0
ahad <ahadg446@gmail.com>
2025-01-03 23:43:53 +0500

    Shifted to separate repos 🔗

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,39 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import AppSection from "./components/AppSection";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import { ClientImages, ServerImages } from "./Images";
-import Loader from "./components/Loader";
 
 const HomePage = () => {
-  const [versionData, setVersionData] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchVersionData = async () => {
-      try {
-        const response = await fetch(
-          "https://api.github.com/repos/ahad324/TransferXUpdates/releases/latest"
-        );
-        const data = await response.json();
-        setVersionData({
-          client: data.assets[0].browser_download_url,
-          server: data.assets[1].browser_download_url,
-        });
-        setError(null);
-      } catch (error) {
-        console.error("Error fetching version data:", error);
-        setError(
-          "There was an issue loading the apps. Please try again later."
-        );
-      }
-    };
-
-    fetchVersionData();
-  }, []);
-
   const clientFeatures = [
     "Files Zipping",
     "Auto-Connect",
@@ -55,28 +28,25 @@ const HomePage = () => {
       <main>
         <Hero />
         <Features />
-        {error ? (
-          <div className="error-message text-center font-bold text-red-500">
-            {error}
-          </div>
-        ) : versionData ? (
-          <>
-            <AppSection
-              type="client"
-              downloadLink={versionData.client}
-              features={clientFeatures}
-              images={ClientImages}
-            />
-            <AppSection
-              type="server"
-              downloadLink={versionData.server}
-              features={serverFeatures}
-              images={ServerImages}
-            />
-          </>
-        ) : (
-          <Loader Message={"Loading Apps..."} />
-        )}
+
+        <>
+          <AppSection
+            type="client"
+            downloadLink={
+              "https://ahad324.github.io/TransferXClient/TransferX.exe"
+            }
+            features={clientFeatures}
+            images={ClientImages}
+          />
+          <AppSection
+            type="server"
+            downloadLink={
+              "https://ahad324.github.io/TransferXServer/TransferXServer.exe"
+            }
+            features={serverFeatures}
+            images={ServerImages}
+          />
+        </>
         <About />
       </main>
       <Footer />
