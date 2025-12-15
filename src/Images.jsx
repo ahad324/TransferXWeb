c6fee42074097fd29b8822bceb94fcb6dc2c18c7
ahad <ahadg446@gmail.com>
2025-12-16 01:13:37 +0500

    Images updated

--- src/Images.jsx
+++ src/Images.jsx
@@ -1,14 +1,17 @@
 import client1 from "/assets/Images/client/1st.webp";
 import client2 from "/assets/Images/client/2nd.webp";
+import client3 from "/assets/Images/client/3rd.webp";
+import client4 from "/assets/Images/client/4th.webp";
 
-export const ClientImages = [client1, client2];
+export const ClientImages = [client1, client2, client3, client4];
 
 import server1 from "/assets/Images/server/1st.webp";
 import server2 from "/assets/Images/server/2nd.webp";
 import server3 from "/assets/Images/server/3rd.webp";
 import server4 from "/assets/Images/server/4th.webp";
+import server5 from "/assets/Images/server/5th.webp";
 
-export const ServerImages = [server1, server2, server3, server4];
+export const ServerImages = [server1, server2, server3, server4, server5];
 
 import rocketImg from "/assets/FeaturesImages/rocket.svg";
 import networkImg from "/assets/FeaturesImages/network.svg";
